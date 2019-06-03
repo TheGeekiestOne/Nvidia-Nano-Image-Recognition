ba445094d10563a7adb9131d76b5382895864224
Ayran Olckers <w1654684@my.westminster.ac.uk>
2019-06-03 12:21:59 +0100

    Update my-recognition.cpp

--- my-recognition/my-recognition.cpp
+++ my-recognition/my-recognition.cpp
@@ -28,11 +28,11 @@ int main( int argc, char** argv )
 	// these variables will be used to store the image data and dimensions
 	// the image data will be stored in shared CPU/GPU memory, so there are
 	// pointers for the CPU and GPU (both reference the same physical memory)
-	float* imgCPU    = NULL;		// CPU pointer to floating-point RGBA image data
-	float* imgCUDA   = NULL;		// GPU pointer to floating-point RGBA image data
+	float* imgCPU    = 0;		// CPU pointer to floating-point RGBA image data
+	float* imgCUDA   = 0;		// GPU pointer to floating-point RGBA image data
 	int    imgWidth  = 0;		// width of the image (in pixels)
 	int    imgHeight = 0;		// height of the image (in pixels)
-		
+
 	// load the image from disk as float4 RGBA (32 bits per channel, 128 bits per pixel)
 	if( !loadImageRGBA(imgFilename, (float4**)&imgCPU, (float4**)&imgCUDA, &imgWidth, &imgHeight) )
 	{
@@ -58,14 +58,14 @@ int main( int argc, char** argv )
 	// this will return the index of the object class that the image was recognized as (or -1 on error)
 	const int classIndex = net->Classify(imgCUDA, imgWidth, imgHeight, &confidence);
 
-	// make sure a valid classification result was returned	
+	// make sure a valid classification result was returned
 	if( classIndex >= 0 )
 	{
 		// retrieve the name/description of the object class index
 		const char* classDescription = net->GetClassDesc(classIndex);
 
 		// print out the classification results
-		printf("image is recognized as '%s' (class #%i) with %f%% confidence\n", 
+		printf("image is recognized as '%s' (class #%i) with %f%% confidence\n",
 			  classDescription, classIndex, confidence * 100.0f);
 	}
 	else
@@ -73,11 +73,10 @@ int main( int argc, char** argv )
 		// if Classify() returned < 0, an error occurred
 		printf("failed to classify image\n");
 	}
-	
+
 	// free the network's resources before shutting down
 	delete net;
 
 	// this is the end of the example!
 	return 0;
 }
-
